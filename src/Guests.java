6afb1d178fe47c24adb8d8c0e528dedb4df384e8
thefluffyoshi <katielynntrinh@gmail.com>
2019-11-06 11:48:26 -0800

    Done

--- src/Guests.java
+++ src/Guests.java
@@ -3,6 +3,7 @@ public class Guests
 {
     // Creates an array of guests' names and allows the user to
     // search the array for a particular name.
+    
     public static void main (String[] args)
     {
         String[] guests = {"Paul", "Wendy", "Jared", "Eric", "Ayame",
@@ -18,7 +19,6 @@ public class Guests
         for (String guest : guests)
             if (name.equals(guest))
                 found = true;
-            
 
         if (found)
             System.out.println (name + " is on the guest list.");
