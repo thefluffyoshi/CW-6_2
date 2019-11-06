4d6dbe7b86e6c73d127beb279d4558a52c810305
thefluffyoshi <katielynntrinh@gmail.com>
2019-11-06 11:46:06 -0800

    Done

---
+++ src/Guests.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Guests
+{
+    // Creates an array of guests' names and allows the user to
+    // search the array for a particular name.
+    public static void main (String[] args)
+    {
+        String[] guests = {"Paul", "Wendy", "Jared", "Eric", "Ayame",
+                            "Ian", "Issobel", "Hakem"};
+        String name;
+        boolean found = false;
+        Scanner scan = new Scanner (System.in);
+
+        System.out.print("Enter a name: ");
+        name = scan.nextLine();
+
+        //Perform a linear search
+        for (String guest : guests)
+            if (name.equals(guest))
+                found = true;
+            
+
+        if (found)
+            System.out.println (name + " is on the guest list.");
+        else
+            System.out.println (name + "is not on the guest list.");
+    }//end main
+}//end class Guests
